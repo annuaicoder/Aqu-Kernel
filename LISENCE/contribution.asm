80e0692199338b1aaf9846dea148cbcda5d51ccf
adminmetricvisio <admin@metricvisio.org>
2024-10-16 15:00:24 -0700

    Add LISENCE Files

---
+++ LISENCE/contribution.asm
@@ -0,0 +1,11 @@
+; Contribution Guidelines
+; Thank you for considering contributing to Aqu Kernel! Here are some guidelines to help you get started.
+; How to Contribute
+; 1. Fork the repository.
+; 2. Create a new branch for your changes (`git checkout -b feature-branch`).
+; 3. Make your changes and commit them (`git commit -m "Describe changes"`).
+; 4. Push to your branch (`git push origin feature-branch`).
+; 5. Submit a pull request.
+; Code of Conduct
+; Be respectful and considerate of other contributors.
+; By contributing to Aqu Kernel, you agree that your contributions will be licensed under the MIT License.
---
+++ LISENCE/copyright.asm
@@ -0,0 +1,2 @@
+; Copyright (c) 2024 Aqu Kernel By Anas. All rights reserved.
+; This file is part of the Aqu Kernel project.
